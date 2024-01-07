1b0731a19049450944d6e27a7b8bbd0e49751f76
Arghya <arghyaghosho48@gmail.com>
2024-01-07 10:42:35 +0530

    week-3 done

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,6 +10,12 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+function reqCount(req,res,next){
+    requestCount=requestCount+1;
+    next();
+}
+app.use(reqCount)
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
